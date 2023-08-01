c6049507baad4d86beb22113d19de5c14508e28f
Geo-M69 <128603618+Geo-M69@users.noreply.github.com>
2023-08-01 11:10:48 -0400

    Update scrolling.js

--- scrolling.js
+++ scrolling.js
@@ -4,14 +4,11 @@ function smoothScroll(target) {
   if (!targetSection) return; // Return if the target section doesn't exist
 
   const targetPosition = targetSection.offsetTop;
-  const startPosition = window.pageYOffset; // Get the current scroll position
+  const startPosition = window.pageYOffset;
   const distance = targetPosition - startPosition;
   const duration = 800; // Adjust the duration (in milliseconds) as needed
 
-  let start = null;
-
   function animationStep(timestamp) {
-    if (!start) start = timestamp;
     const progress = timestamp - start;
     const percentage = Math.min(progress / duration, 1);
 
@@ -27,6 +24,10 @@ function smoothScroll(target) {
     return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
   };
 
-  // Start the animation immediately
+  let start = null;
+  // Set the start timestamp before calling requestAnimationFrame
+  start = window.performance.now();
+
+  // Start the animation
   requestAnimationFrame(animationStep);
 }
